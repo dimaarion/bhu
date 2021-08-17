b5433adbf0b28dc535cd900c472cd87e7a6d2f3a
dimaarion <dimaarion@gmail.com>
2021-08-17 07:25:15 +0300

    dimaarion/sandaniprim.git

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
 import Article from "./Article";
-
 export default function Pages(props) {
-   
-    const SELECARTICLES = useSelector((store)=>store.getArticles);
 
-   
+    const SELECARTICLES = useSelector((store) => store.getArticles);
+    const LANG = useSelector((store)=>store.lang);
+
 
     return (
-        <div>
-           {SELECARTICLES.filter((f)=>"/" + f.art_alias === props.location.pathname).map((art)=><Article countArt = "1" name = {art.art_names} alias = {art.art_alias} subContent = {art.art_subcontent} content = {art.art_content}/>) }
+        <div className="row mt-3">
+            <div className="col-1"></div>
+            <div className="col-10 ">
+                {SELECARTICLES.filter((f) => "/" + f.art_alias === props.location.pathname && f.art_lang === LANG).map((art) => <Article countArt="1" name={art.art_names} alias={art.art_alias} subContent={art.art_subcontent} content={art.art_content} />)}
+            </div>
+            <div className="col-1"></div>
         </div>
     )
 }
